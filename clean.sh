a97021b6ba16fde3a368f8e421fa42f894dc1445
Terence Zama <terencez@wigroup.co.za>
2018-04-23 13:38:09 +0400

    cool

--- clean.sh
+++ clean.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+bundle_id=com.terence.cenacle
 # rm -rf $TMPDIR/react-*
 # npm start -- --reset-cache
+
 
 rm -rf ios
 rm -rf android
@@ -12,8 +13,12 @@ npm install
 react-native link
 
 # watchman watch-del-all
+
+# ios works
+plutil -replace CFBundleIdentifier -string $bundle_id ios/cenacle/Info.plist
 cp _clean/AppDelegate.m ios/cenacle/AppDelegate.m
 cp _clean/Podfile ios/Podfile
+cp _clean/GoogleService-Info.plist ios/cenacle
 cd ios
 pod install
 cd ../
